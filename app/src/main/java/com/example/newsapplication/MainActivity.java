f871785f136cd39d45d2644b092805c8ff744859
Guilherme Sussel <guilherme.sussel@fast2mine.com>
2023-05-01 17:43:31 -0300

    added swipe-down-to-refresh to main fragment, layout fixes

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.newsapplication;
 
 import android.os.Bundle;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 import com.example.newsapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-private ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/newsapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/newsapplication/ui/home/HomeFragment.java
@@ -1,19 +1,23 @@
 package com.example.newsapplication.ui.home;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.newsapplication.databinding.FragmentHomeBinding;
 import com.example.newsapplication.ui.news.Article;
 import com.example.newsapplication.ui.news.adapter.NewsAdapter;
 import com.example.newsapplication.ui.news.NewsResponse;
 import com.example.newsapplication.ui.news.service.NewsService;
-import com.example.newsapplication.databinding.FragmentHomeBinding;
 
 import java.util.List;
 
@@ -23,10 +27,12 @@ import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     private FragmentHomeBinding binding;
 
     private String newsSource = "bbc-news";
-    private String apiKey = "";
+    private String apiKey = "80eb8c43c5464f7b90f05e5fcdee58dd";
 
     private ListView listViewNews;
 
@@ -37,6 +43,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        swipeRefreshLayout = binding.swipeRefreshLayout;
+
+        // Set the OnRefreshListener on the SwipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            // Refresh the data
+            fetchNews();
+        });
+
         listViewNews = binding.listViewNewsList;
 
         fetchNews();
@@ -46,12 +60,29 @@ public class HomeFragment extends Fragment {
 
     private void fetchNews() {
         Call<NewsResponse> call = newsService.getTopHeadlines(newsSource, apiKey);
+
+        // Create a ProgressBar
+        ProgressBar progressBar = new ProgressBar(getContext(), null, android.R.attr.progressBarStyleLarge);
+        progressBar.setIndeterminate(true);
+
+        // Create a Dialog with the ProgressBar as the content view
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(progressBar);
+        builder.setCancelable(false);
+        builder.setTitle("Fething news...");
+        AlertDialog dialog = builder.create();
+
+        // Show the dialog
+        dialog.show();
         call.enqueue(new Callback<NewsResponse>() {
             @Override
             public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
                 if (response.isSuccessful()) {
                     List<Article> listOfArticles = response.body().getArticles();
                     setNewsListAdapter(listOfArticles);
+                    dialog.dismiss();
+                    // Notify the SwipeRefreshLayout that the refresh is complete
+                    swipeRefreshLayout.setRefreshing(false);
                 } else {
                     // TODO handle error case
                 }
@@ -60,6 +91,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onFailure(Call<NewsResponse> call, Throwable t) {
                 // TODO handle failed request
+                dialog.dismiss();
+                // Notify the SwipeRefreshLayout that the refresh is complete
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
